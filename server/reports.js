419cd48b347dbbd61ded4a571d00642fe3c6e39e
Matthew Favetti <mpfavetti@ucdavis.edu>
2023-08-31 15:05:36 -0700

    Adding first report

---
+++ server/reports.js
@@ -0,0 +1,119 @@
+import { open } from 'node:fs/promises';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config';
+
+import c from './collector.js';
+import general from './reports_helpers/general.js';
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+c.off(); // disable debugging
+
+const reports = (ids) => {
+	general(ids);
+}
+
+const fileProjects = async () => {
+	if(process.argv.length !== 4) {
+		usage();
+		return;
+	}
+
+	const infilepath = process.argv[3];
+
+	let infile;
+
+	try {
+		infile = await open(infilepath);
+
+		console.log(`Getting projects ids from: ${infilepath}`);
+
+		const lines = [];
+
+		for await(const l of infile.readLines()) {
+			lines.push(l);
+		}
+
+		if(lines.length < 2) {
+			console.log('No project ids found');
+			return;
+		}
+
+		reports(lines.slice(1));
+	}
+	catch (e) {
+		if(e.code === 'ENOENT') {
+			console.log('File not found');
+		}
+		else {
+			throw e;
+		}
+
+	}
+	finally {
+		await infile?.close();
+	}
+}
+
+const allProjects = async () => {
+	console.log(`Getting project ids from db`);
+
+	try {
+		const db = client.db('bctool');
+		const projects = db.collection('projects');
+		const opts = {
+			projection: {
+				'_id': 1,
+			},
+		};
+		const cursor = await projects.find({}, opts);
+		const ids = [];
+
+		for await (const project of cursor) {
+			ids.push(project._id.toString());
+		}
+
+		reports(ids);
+	}
+	finally {
+		await client.close();
+	}
+}
+
+const oneProject = () => {
+	if(process.argv.length !== 4) {
+		usage();
+		return;
+	}
+
+	reports([process.argv[3]]);
+}
+
+const usage = () => {
+	console.log(`usage: reports [--help, -h] [--file, -f filename] [--all, -a] [--project, -p id]\n`)
+	console.log(`-h, --help\t display this message and exit`)
+	console.log(`-f, --file\t generate reports for projects from file of ids (one per line, skip first line for header)`)
+	console.log(`-a, --all\t generate reports for all projects`)
+	console.log(`-p, --project\t generate reports for a project by id`)
+}
+
+if(process.argv.length > 2) {
+	switch(process.argv[2]) {
+		case '-f':
+		case '--file':
+			fileProjects();
+		break;
+		case '-a':
+		case '--all':
+			allProjects()
+		case '-p':
+		case '--project':
+			oneProject()
+		break;
+		default:
+			usage();
+	}
+}
+else {
+	usage();
+}
---
+++ server/reports_helpers/general.js
@@ -0,0 +1,130 @@
+import dotenv from 'dotenv';
+import { MongoClient, ObjectId } from 'mongodb';
+import { BSONTypeError } from 'bson';
+
+import writeCSV from '../debug_helpers/writeCSV.js';
+import calcProjectLength from '../benefits_helpers/calcProjectLength.js';
+import calcLength from '../benefits_helpers/safety/calcLength.js';
+import avgProp from '../benefits_helpers/avgProp.js';
+
+dotenv.config({path: '../.env'});
+
+const general = async (ids) => {
+
+	console.log('Starting general report');
+
+	console.log(`Looking up ${ids.length} projects`);
+
+	const headers = [
+		'Project ID',
+		'Average bicycle exposure (ways)',
+		'Average bicycle exposure (intersections)',
+		'Average pedestrian exposure (ways)',
+		'Average pedestrian exposure (intersections)',
+		'Average bicycle demand',
+		'Average pedestrian demand',
+		'Total project length',
+		'Total project length used for safety calculations',
+	];
+
+	const rows = [];
+
+	let client;
+
+	try {
+
+		client = new MongoClient(process.env.MONGO_URI);
+	    const db = client.db('bctool');
+		const projects = db.collection('projects');
+
+		for(const projectId of ids) {
+
+			try {
+				const project = await projects.findOne({
+					'_id': new ObjectId(projectId),
+				});
+
+				if(project === null) {
+					console.log(`Couldn't find project with id: ${projectId}`);
+					continue;
+				}
+
+				const projectLength = calcProjectLength(
+					project.scope.segments,
+					project.scope.userSegments
+				);
+
+				const safetyLength = calcLength(
+					project.elements.infrastructure,
+					projectLength
+				);
+
+				let avgBikeExpWay = avgProp(
+					project.scope.segments,
+					'bicyclist_link_exposure'
+				);
+
+				let avgBikeExpInt = avgProp(
+					project.scope.intersections,
+					'bicycle_node_exposure'
+				);
+
+				let avgPedExpWay = avgProp(
+					project.scope.segments,
+					'pedestrian_link_exposure'
+				);
+
+				let avgPedExpInt = avgProp(
+					project.scope.intersections,
+					'pedestrian_node_exposure'
+				);
+
+				let avgBikeDemand = avgProp(
+					project.scope.segments,
+					'bicyclist_demand'
+				);
+
+				let avgPedDemand = avgProp(
+					project.scope.intersections,
+					'ped_demand'
+				);
+
+				avgBikeExpWay = avgBikeExpWay === null ? 'No network exposure available' : avgBikeExpWay;
+				avgBikeExpInt = avgBikeExpInt === null ? 'No network exposure available' : avgBikeExpInt;
+
+				avgPedExpWay = avgPedExpWay === null ? 'No network exposure available' : avgPedExpWay;
+				avgPedExpInt = avgPedExpInt === null ? 'No network exposure available' : avgPedExpInt;
+
+				avgBikeDemand = avgBikeDemand === null ? 'No network demand available' : avgBikeDemand;
+				avgPedDemand = avgPedDemand === null ? 'No network demand available' : avgPedDemand;
+
+				rows.push([
+					project._id,
+					avgBikeExpWay,
+					avgBikeExpInt,
+					avgPedExpWay,
+					avgPedExpInt,
+					project.scope.segments.length ? avgBikeDemand : 'No network ways selected',
+					project.scope.intersections.length ? avgPedDemand : 'No network intersections selected',
+					projectLength,
+					safetyLength,
+				]);
+			}
+			catch (e) {
+				if(e instanceof BSONTypeError) {
+					console.log(`Invalid project id: ${projectId}`);
+				}
+				else {
+					throw e;
+				}
+			}
+		}
+	}
+	finally {
+		await client?.close();
+	}
+
+	writeCSV('reports', 'general', headers, rows);
+}
+
+export default general;
