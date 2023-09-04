bf502aed7399257d503ef80185b71d7b66a245a6
Matthew Favetti <mpfavetti@ucdavis.edu>
2023-09-04 01:20:11 -0700

    Updating usage for debug

--- server/debug.js
+++ server/debug.js
@@ -1,3 +1,4 @@
+import { open } from 'node:fs/promises';
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 
@@ -13,12 +14,10 @@ import safety from './helpers/debug/safety.js';
 
 dotenv.config();
 
-clean('debug');
-clean('lookups');
-
 const client = new MongoClient(process.env.MONGO_URI);
 
 const debug = (project) => {
+	console.log(`Debugging ${project._id.toString()}`);
 	c.reset();
 	inputs(project);
 	reach(project);
@@ -26,12 +25,108 @@ const debug = (project) => {
 	safety(project);
 };
 
-lookups();
-constants();
+const allProjects = async () => {
 
-if(process.argv.length === 3) {
+	console.log(`Debugging all projects in db`);
 
-	const projectId = process.argv[2]
+	clean('debug');
+	clean('lookups');
+
+	try {
+
+	    const db = client.db('bctool');
+		const projects = db.collection('projects');
+		const cursor = await projects.find({});
+
+		for await (const project of cursor) {
+			debug(project);
+		}
+
+		lookups();
+		constants();
+	}
+	finally {
+		await client.close();
+	}
+}
+
+const fileProjects = async () => {
+	if(process.argv.length !== 4) {
+		usage();
+		return;
+	}
+
+	const infilepath = process.argv[3];
+
+	let infile;
+
+	try {
+		infile = await open(infilepath);
+
+		console.log(`Getting projects ids from: ${infilepath}`);
+
+		const lines = [];
+
+		for await(const l of infile.readLines()) {
+			lines.push(l);
+		}
+
+		if(lines.length < 2) {
+			console.log('No project ids found');
+			return;
+		}
+
+		try {
+
+			clean('debug');
+			clean('lookups');
+
+		    const db = client.db('bctool');
+			const projects = db.collection('projects');
+
+			for(let projectId of lines.slice(1)) {
+
+				const project = await projects.findOne({
+					'_id': new ObjectId(projectId),
+				});
+
+				if(project === null) {
+					console.log(`Couldn't find project with id ${projectId}`);
+					continue;
+				}
+
+				debug(project);
+			}
+
+			lookups();
+			constants();
+		}
+		finally {
+			await client.close();
+		}
+	}
+	catch (e) {
+		if(e.code === 'ENOENT') {
+			console.log('File not found');
+		}
+		else {
+			throw e;
+		}
+
+	}
+	finally {
+		await infile?.close();
+	}
+};
+
+const oneProject = async () => {
+
+	if(process.argv.length !== 4) {
+		usage();
+		return;
+	}
+
+	const projectId = process.argv[3];
 
 	try {
 
@@ -43,28 +138,46 @@ if(process.argv.length === 3) {
 
 		if(project === null) {
 			console.log(`Couldn't find project with id ${projectId}`);
-			process.exit()
+			process.exit();
 		}
 
+		clean('debug');
+		clean('lookups');
 		debug(project);
+		lookups();
+		constants();
 	}
 	finally {
 		await client.close();
 	}
 }
+
+const usage = () => {
+	console.log(`usage: debug [--help, -h] [--file, -f filename] [--all, -a] [--project, -p id]\n`)
+	console.log(`-h, --help\t display this message and exit`)
+	console.log(`-f, --file\t generate debugging output for projects from file of ids (one per line, skip first line for header)`)
+	console.log(`-a, --all\t generate debugging output for all projects`)
+	console.log(`-p, --project\t generate debugging output for a project by id`)
+}
+
+if(process.argv.length > 2) {
+	switch(process.argv[2]) {
+		case '-f':
+		case '--file':
+			fileProjects();
+		break;
+		case '-a':
+		case '--all':
+			allProjects();
+		break;
+		case '-p':
+		case '--project':
+			oneProject()
+		break;
+		default:
+			usage();
+	}
+}
 else {
-	try {
-
-	    const db = client.db('bctool');
-		const projects = db.collection('projects');
-		const cursor = await projects.find({});
-
-		for await (const project of cursor) {
-			console.log(`Debugging ${project._id.toString()}`);
-			debug(project);
-		}
-	}
-	finally {
-		await client.close();
-	}
+	usage();
 }
