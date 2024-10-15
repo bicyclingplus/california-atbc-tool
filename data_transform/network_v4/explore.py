c0be07572894a74252970de0e1e72abf2aef6b82
Matthew Favetti <mpfavetti@ucdavis.edu>
2024-10-15 15:46:54 -0700

    working on new network

--- data_transform/network_v4/explore.py
+++ data_transform/network_v4/explore.py
@@ -1,10 +1,12 @@
 import json
 import os
 
-infilename = 'bike_predictions_strava_v1.geojson'
+# infilename = 'bike_predictions_strava_v1.geojson'
+# infilename = 'Study_Area_Bike_Output_job_pop_v1.geojson'
+infilename = 'Study_Area_Bike_Output_job_pop_v2.geojson'
 infilepath = os.path.join('input', infilename)
 
 geojson = json.load(open(infilepath))
 
-for i in range(10):
-    print(json.dumps(geojson['features'][i]['properties']))
+for i in range(1):
+    print(json.dumps(geojson['features'][i]))
