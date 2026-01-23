4b7f537a6340229dac80e2ca6a77d42d55dcf4db
Matthew Favetti <mpfavetti@ucdavis.edu>
2026-01-22 16:56:09 -0800

    adding lookup table for pm25 mortality rates

---
+++ data_transform/emissions/pm25_mortality.py
@@ -0,0 +1,18 @@
+import csv
+import json
+import os
+
+input_file = 'PM25_ITHIM_full_interpolated_2020_2050.csv'
+output_file = 'pm25_mortality.json'
+
+data = {}
+
+with open(os.path.join('input', input_file)) as infile:
+	reader = csv.reader(infile)
+	headers = next(reader)
+	for r in reader:
+		year = r[headers.index('year')]
+		rate = float(r[headers.index('pm25_mortality_per_ton')])
+		data[year] = rate
+
+json.dump(data, open(os.path.join('output', output_file), 'w'))
