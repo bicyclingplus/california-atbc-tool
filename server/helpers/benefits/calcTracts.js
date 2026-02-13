148e1571e33f1e352dc78a37f9c470a7bfd812c6
Matthew Favetti <mpfavetti@ucdavis.edu>
2026-02-12 17:03:50 -0800

    querying tracts based on buffer bbox for simpler geometry

--- server/helpers/benefits/calcTracts.js
+++ server/helpers/benefits/calcTracts.js
@@ -27,25 +27,40 @@ const calc = async (
 
   // figure out which tracts intersect with the bike buffer
   let bike_buffer = turf.buffer(features, 1.00, {units: 'miles'});
+
+  // the buffer will be a a polygon for each feature,
+  // merge into a single polygon
   bike_buffer = turf.union(bike_buffer);
+
+  // query on the simpler geometry of the bounding box
+  const bike_bbox = turf.bbox(bike_buffer);
+  const bike_bbox_poly = turf.bboxPolygon(bike_bbox);
 
   const client = new MongoClient(process.env.MONGO_URI);
 
-  const bike_tracts = await client
+  const all_tracts = await client
     .db('bctool')
     .collection('tracts')
     .find({
       geometry: {
         "$geoIntersects": {
-          "$geometry": bike_buffer.geometry,
+          "$geometry": bike_bbox_poly.geometry,
         }
       },
     })
     .toArray();
 
-  // since the walk buffer is smaller, don't query the database
-  // again, just calculate which tracts also intersect the
-  // smaller buffer
+  // figure out which tracts are within the bike buffer
+  const bike_tracts = [];
+
+  for(const tract of all_tracts) {
+    if(turf.intersect(turf.featureCollection([tract, bike_buffer])) !== null) {
+      bike_tracts.push(tract);
+    }
+  }
+
+  // since the walk buffer is smaller, just calculate which
+  // bike tracts also intersect the walk buffer
   let walk_buffer = turf.buffer(features, 0.25, {units: 'miles'});
   walk_buffer = turf.union(walk_buffer);
 
