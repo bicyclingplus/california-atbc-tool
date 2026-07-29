7c14cc139cf5e102b30b8bbdc2d97a0c3efa1bed
Matthew Favetti <mpfavetti@ucdavis.edu>
2026-07-29 14:17:50 -0700

    more work on new network

--- data_transform/network_v5/explore.py
+++ data_transform/network_v5/explore.py
@@ -18,7 +18,7 @@ geojson = json.load(open(infilepath))
 for i in range(1):
     print(json.dumps(geojson['features'][i]))
 
-infilename = 'context_blocks.geojson'
+infilename = '2026_07_29_context_blocks.geojson'
 infilepath = os.path.join('input', infilename)
 
 geojson = json.load(open(infilepath))
--- data_transform/network_v5/insert.py
+++ data_transform/network_v5/insert.py
@@ -7,6 +7,17 @@ dbname = client['bctool']
 
 print('Starting ways')
 
+goodprops = [
+    "edge_uid",
+    "source",
+    "target",
+    "functional",
+    "bicycle_exposure_class",
+    "pedestrian_exposure_class",
+    "pred_bike_vol",
+    "pred_ped_vol",
+]
+
 collection_name = dbname['ways']
 collection_name.delete_many({})
 collection_name.drop_indexes()
@@ -17,6 +28,14 @@ way_files = [
 
 for file in way_files:
     geojson = json.load(open(os.path.join('input', file)))
+
+    for feature in geojson['features']:
+        newprops = {}
+        for p in feature['properties']:
+            if p in goodprops:
+                newprops[p] = feature['properties'][p]
+        feature['properties'] = newprops
+
     collection_name.insert_many(geojson['features'])
 
 indexes = [
@@ -30,6 +49,15 @@ collection_name.create_indexes(indexes)
 
 print('Starting intersections')
 
+goodprops = [
+    "node_id",
+    "functional",
+    "bicycle_exposure_class",
+    "pedestrian_exposure_class",
+    "pred_bike_vol",
+    "pred_ped_vol",
+]
+
 collection_name = dbname['intersections']
 collection_name.delete_many({})
 collection_name.drop_indexes()
@@ -40,6 +68,14 @@ intersection_files = [
 
 for file in intersection_files:
     geojson = json.load(open(os.path.join('input', file)))
+
+    for feature in geojson['features']:
+        newprops = {}
+        for p in feature['properties']:
+            if p in goodprops:
+                newprops[p] = feature['properties'][p]
+        feature['properties'] = newprops
+
     collection_name.insert_many(geojson['features'])
 
 indexes = [
@@ -51,16 +87,29 @@ collection_name.create_indexes(indexes)
 
 print('Starting blocks')
 
+goodprops = [
+    "pred_bike_vol_newpath",
+    "pred_ped_vol_newpath",
+]
+
 collection_name = dbname['blocks']
 collection_name.delete_many({})
 collection_name.drop_indexes()
 
 blocks_files = [
-    "2026_07_24_context_blocks.geojson",
+    "2026_07_29_context_blocks.geojson",
 ]
 
 for file in blocks_files:
     geojson = json.load(open(os.path.join('input', file)))
+
+    for feature in geojson['features']:
+        newprops = {}
+        for p in feature['properties']:
+            if p in goodprops:
+                newprops[p] = feature['properties'][p]
+        feature['properties'] = newprops
+
     collection_name.insert_many(geojson['features'])
 
 indexes = [
